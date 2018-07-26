b7298157555f21e60792aaf337e248e6e42c9a62
Adityaraj1711 <adityamailbox0@gmail.com>
2018-07-26 15:18:36 -0700

    wsgi

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -15,15 +15,15 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 #application = get_wsgi_application()
 
-#from whitenoise.django import DjangoWhiteNoise
-#application = DjangoWhiteNoise(application)
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from django.core.wsgi import get_wsgi_application
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
